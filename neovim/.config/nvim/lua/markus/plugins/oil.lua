8f119438d3abb20894344aea794107816ea88163
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-01-23 11:23:55 +0100

    nvim: add oil

---
+++ neovim/.config/nvim/lua/markus/plugins/oil.lua
@@ -0,0 +1,30 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+    })
+  end
+}
+
