8cbb93e3622f328af5a3d721007eff8f1ded4f7b
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-02-21 13:41:32 +0100

    nvim: fix tree keymaps

--- neovim/.config/nvim/lua/markus/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/markus/plugins/nvim-tree.lua
@@ -1,35 +1,40 @@
 -- -- nvim.tree
--- local api = require("nvim-tree.api")
---
--- local function edit_or_open()
---   local node = api.tree.get_node_under_cursor()
---
---   if node.nodes ~= nil then
---     -- expand or collapse folder
---     api.node.open.edit()
---   else
---     -- open file
---     api.node.open.edit()
---     -- Close the tree if file was opened
---     api.tree.close()
---   end
--- end
---
--- -- open as vsplit on current node
--- local function vsplit_preview()
---   local node = api.tree.get_node_under_cursor()
---
---   if node.nodes ~= nil then
---     -- expand or collapse folder
---     api.node.open.edit()
---   else
---     -- open file as vsplit
---     api.node.open.vertical()
---   end
---
---   -- Finally refocus on tree if it was lost
---   api.tree.focus()
--- end
+
+local function my_on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- function for left to assign to keybindings
+  local lefty = function ()
+    local node_at_cursor = api.tree.get_node_under_cursor()
+    -- if it's a node and it's open, close
+    if node_at_cursor.nodes and node_at_cursor.open then
+      api.node.open.edit()
+    -- else left jumps up to parent
+    else
+      api.node.navigate.parent()
+    end
+  end
+
+  -- function for right to assign to keybindings
+  local righty = function ()
+    local node_at_cursor = api.tree.get_node_under_cursor()
+    -- if it's a closed node, open it
+    if node_at_cursor.nodes and not node_at_cursor.open then
+      api.node.open.edit()
+    end
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '?', api.tree.toggle_help,  opts('Help'))
+  vim.keymap.set("n", "h", lefty , opts("Close node") )
+  vim.keymap.set("n", "<Left>", lefty , opts("Close node") )
+  vim.keymap.set("n", "<Right>", righty , opts("Open node") )
+  vim.keymap.set("n", "l", righty , opts("Open node") )
+end
 
 
 return {
@@ -44,11 +49,6 @@ return {
       view = {
         width = 45,
         relativenumber = true,
-        -- mappings = {
-        --   list = {
-        --     { key = "l", cb = edit_or_open "open" },
-        --   },
-        -- },
       },
 
       -- change folder arrow icons
@@ -81,13 +81,8 @@ return {
       git = {
         ignore = false,
       },
+      on_attach = my_on_attach,
     }
   end,
 }
-
---vim.keymap.set("n", "l", edit_or_open,          {desc="Edit Or Open"})
---vim.keymap.set("n", "L", vsplit_preview,        {desc="Vsplit Preview"})
---vim.keymap.set("n", "h", api.tree.close,        {desc="Close"})
---vim.keymap.set("n", "H", api.tree.collapse_all, {desc="Collapse All"})
-
 
