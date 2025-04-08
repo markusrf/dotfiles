3003b77d1c935c107e3cb7be7818aca3532aea8a
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-04-08 09:47:46 +0200

    nvim: add mini.splitjoin

---
+++ neovim/.config/nvim/lua/markus/plugins/mini.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.pairs",
+    version = "*",
+    config = function ()
+      require("mini.pairs").setup({
+      })
+    end,
+  },
+
+  {
+    "echasnovski/mini.splitjoin",
+    version = '*',
+    config = function ()
+      require("mini.splitjoin").setup({})
+    end
+  },
+}
+
