c979f7c245fd408c3eae8a84fd60b3ff8a446862
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-04-15 11:04:10 +0200

    nvim: refactor options.lua

--- neovim/.config/nvim/lua/markus/core/options.lua
+++ neovim/.config/nvim/lua/markus/core/options.lua
@@ -5,31 +5,25 @@ vim.cmd.colorscheme "catppuccin-macchiato"
 -- vim.cmd.colorscheme "flexoki-dark"
 
 local opt = vim.opt
+
 opt.number = true
 opt.relativenumber = true
-opt.tabstop = 2         -- Number of spaces per tab
-opt.shiftwidth = 2      -- Indentation width
-opt.expandtab = true    -- Convert tabs to spaces
-opt.autoindent = true   -- Copy indent of current line to new line
-opt.smartindent = true  -- Auto-indent new lines
 opt.termguicolors = true -- Enable true color support
 opt.scrolloff = 3
 
-
-opt.wrap = false
+opt.tabstop = 2        -- Number of spaces per tab
+opt.shiftwidth = 2     -- Indentation width
+opt.expandtab = true   -- Convert tabs to spaces
+opt.autoindent = true  -- Copy indent of current line to new line
+opt.smartindent = true -- Auto-indent new lines
 
 -- search settings
-opt.ignorecase = true -- case insensitive search
-opt.smartcase = true -- uses mixed case for case sensitive search
+opt.ignorecase = true               -- case insensitive search
+opt.smartcase = true                -- uses mixed case for case sensitive search
 
-opt.cursorline = true
-
-opt.background = "dark" -- colorschemes can be light or dark
-opt.signcolumn = "yes" -- show sign column so that text doesnt shift
-
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position 
-
--- clipboard
+opt.wrap = false                    -- textwrap
+opt.cursorline = true               -- highline the entire line the cursor is on
+opt.background = "dark"             -- colorschemes can be light or dark
+opt.signcolumn = "yes"              -- show sign column so that text doesnt shift
+opt.backspace = "indent,eol,start"  -- allow backspace on indent, end of line or insert mode start position
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-
