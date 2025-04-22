f92e3b3a573bbf796f87c203de40eee232798164
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-04-22 13:11:15 +0200

    nvim: add more basedpyright config

--- neovim/.config/nvim/lua/markus/plugins/lsp.lua
+++ neovim/.config/nvim/lua/markus/plugins/lsp.lua
@@ -28,7 +28,7 @@ local on_attach = function(_, bufnr)
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
@@ -161,6 +161,9 @@ return {
               basedpyright = {
                 analysis = {
                   typeCheckingMode = "off",
+                  diagnosticMode = "openFilesOnly",
+                  autoSearchPaths = true,
+                  useLibraryCodeForTypes = true,
                   extraPaths = {
                   },
                 }
