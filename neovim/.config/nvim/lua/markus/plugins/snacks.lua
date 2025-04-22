43de038269e0827c1e837eab8f832566ea0bf67a
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-04-22 13:11:30 +0200

    nvim: snacks config

--- neovim/.config/nvim/lua/markus/plugins/snacks.lua
+++ neovim/.config/nvim/lua/markus/plugins/snacks.lua
@@ -25,12 +25,14 @@ return {
     indent = { enabled = true },
     -- input = { enabled = true },
     -- git = { enabled = true },
-    picker = { enabled = true },
+    -- picker = { enabled = true },
     -- notifier = { enabled = true },
     quickfile = { enabled = true },
     -- scroll = { enabled = false },
     statuscolumn = { enabled = true },
     -- words = { enabled = true },
+    animate = { enabled = true },
+    bufdelete = { enabled = true },
   }
   -- keys = {
   --   { "<leader>sf",       function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
