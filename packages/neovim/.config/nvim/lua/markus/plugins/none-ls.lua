7e7104257a608471f2e6e0b20d3cd3ecd357a8d9
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-06-27 11:30:35 +0200

    nvim: add none-ls

--- packages/neovim/.config/nvim/lua/markus/plugins/none-ls.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/none-ls.lua
@@ -1,15 +1,17 @@
 return {
-  -- "nvimtools/none-ls.nvim",
-  -- dependencies = { "nvim-lua/plenary.nvim" },
-  -- config = function()
-  --   local null_ls = require("null-ls")
-  --
-  --   null_ls.setup({
-  --     sources = {
-  --       -- null_ls.builtins.diagnostics.ruff,
-  --       -- null_ls.builtins.formatting.ruff,
-  --     }
-  --   })
-  -- end
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        -- null_ls.builtins.diagnostics.ruff,
+        -- null_ls.builtins.formatting.ruff,
+        null_ls.builtins.formatting.stylelint,
+        null_ls.builtins.formatting.prettier,
+      }
+    })
+  end
 }
 
