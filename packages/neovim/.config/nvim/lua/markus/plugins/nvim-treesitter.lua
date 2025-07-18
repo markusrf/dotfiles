0b19a6cb0890f2217bfbd6788fa257d029e16647
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-07-18 10:35:16 +0200

    nvim: add render-markdown plugin

--- packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
@@ -13,6 +13,7 @@ return {
         "python",
         "terraform",
         "markdown",
+        "markdown_inline",
         "json",
         "yaml",
         "dockerfile",
@@ -43,7 +44,7 @@ return {
             return true
           end
 
-          local max_filesize = 100 * 1024           -- 100 KB
+          local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             vim.notify(
