cf8ce39f4c6996a5c62813f3b8bb5fb2773437d2
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-09-05 13:46:24 +0200

    nvim: update render markdown sign column

--- packages/neovim/.config/nvim/lua/markus/plugins/render-markdown.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/render-markdown.lua
@@ -6,4 +6,11 @@ return {
   ---@module "render-markdown"
   ---@type render.md.UserConfig
   opts = {},
+  config = function()
+    require("render-markdown").setup({
+      code = {
+        sign = false,
+      },
+    })
+  end
 }
