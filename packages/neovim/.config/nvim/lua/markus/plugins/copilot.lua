4f8e2270bab57daa762a56f38d16a1290ff8d5a7
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-10-03 08:31:27 +0200

    nvim: add copilot plugin

---
+++ packages/neovim/.config/nvim/lua/markus/plugins/copilot.lua
@@ -0,0 +1,26 @@
+return {
+  "zbirenbaum/copilot.lua",
+  dependencies = { "copilotlsp-nvim/copilot-lsp" },
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-enter>",
+          accept_word = "<C-S-enter>",
+          accept_line = false,
+          next = "<C-S-j>",
+          prev = "<C-S-k>",
+          dismiss = "<C-Esc>",
+        },
+      }
+    })
+    vim.keymap.set("n", "<leader>ct", function()
+        require("copilot.suggestion").toggle_auto_trigger()
+      end,
+      { desc = "Toggle Copilot auto suggestion" })
+  end,
+}
