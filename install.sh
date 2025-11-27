977e9ef6213e566cb4132e15eebc207bdf9f5d61
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2025-11-27 16:10:45 +0100

    update install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # install brew
 if ! command -v brew >/dev/null 2>&1; then
-  echo "Installing homebrew"
+  echo "\nInstalling homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   BREW="$(brew --prefix 2>/dev/null || echo '/opt/homebrew')/bin/brew"
   if [[ ! -e "$BREW" ]]; then
@@ -26,8 +26,9 @@ fi
 
 # install oh-my-zsh
 if [[ -z "${ZSH+x}" ]]; then
-  echo "Installing oh my zsh"
+  echo "\nInstalling oh my zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  rm ~/.zshrc  # remove the default .zshrc
   mkdir -p ~/.oh-my-zsh/completions
 fi
 ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
@@ -35,40 +36,34 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
 # install zsh plugins
 if [[ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]]; then
-  echo "Installing p10k"
+  echo "\nInstalling p10k"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
 if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
-  echo "Installing zsh-autosuggestions"
+  echo "\nInstalling zsh-autosuggestions"
   git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]; then
-  echo "Installing zsh-syntax-highlighting"
+  echo "\nInstalling zsh-syntax-highlighting"
   git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
 if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-vi-mode" ]]; then
-  echo "Installing zsh-vi-mode"
+  echo "\nInstalling zsh-vi-mode"
   git clone --depth=1 https://github.com/jeffreytse/zsh-vi-mode.git $ZSH_CUSTOM/plugins/zsh-vi-mode
 fi
 if [[ ! -d "$ZSH_CUSTOM/plugins/fzf-tab" ]]; then
-  echo "Installing fzf-tab"
+  echo "\nInstalling fzf-tab"
   git clone --depth=1 https://github.com/Aloxaf/fzf-tab.git $ZSH_CUSTOM/plugins/fzf-tab
 fi
 
 
 # stow packages
-echo "Restowing packages"
+echo "\nRestowing packages"
 rm -f **/.DS_Store
 rm -f ~/.config/**/.DS_Store
 (cd packages; stow -t ~ -R *)
 
 
-if ! gh extension list | grep -q "dlvhdr/gh-dash"; then
-  echo "Installing gh extension gh-dash"
-  gh extension install dlvhdr/gh-dash
-fi
-
-
 # to make bat know about extra themes
-echo "Rebuilding bat cache"
+echo "\nRebuilding bat cache"
 bat cache --build
