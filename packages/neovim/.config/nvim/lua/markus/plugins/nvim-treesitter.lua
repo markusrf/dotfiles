bbffbe0ba4bd09806e5ed224254cd1e4f2329617
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-01-05 15:45:27 +0100

    nvim: fix treesitter after migrating to main

--- packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
@@ -1,77 +1,64 @@
+local parsers = {
+  "vimdoc",
+  "javascript",
+  "typescript",
+  "lua",
+  "bash",
+  "python",
+  "terraform",
+  "markdown",
+  "markdown_inline",
+  "json",
+  "yaml",
+  "dockerfile",
+  "gitignore",
+  "git_config",
+  "sql",
+  "toml",
+  "vhs",
+  "zsh",
+}
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").setup({
-      -- A list of parser names, or "all"
-      ensure_installed = {
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "lua",
-        "bash",
-        "python",
-        "terraform",
-        "markdown",
-        "markdown_inline",
-        "json",
-        "yaml",
-        "dockerfile",
-        "gitignore",
-        "git_config",
-        "sql",
-        "toml",
-        "vhs",
-        "zsh",
-      },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter")
+      treesitter.setup({})
+      treesitter.install(parsers)
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      indent = {
-        enable = true
-      },
-
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        disable = function(lang, buf)
-          if lang == "html" then
-            print("disabled")
-            return true
-          end
-
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            vim.notify(
-              "File larger than 100KB treesitter disabled for performance",
-              vim.log.levels.WARN,
-              { title = "Treesitter" }
-            )
-            return true
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          if
+              vim.list_contains(
+                treesitter.get_installed(),
+                vim.treesitter.language.get_lang(args.match)
+              )
+          then
+            vim.treesitter.start(args.buf)
+            -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            -- vim.wo.foldmethod = 'expr'
+            -- indentation, provided by nvim-treesitter
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
           end
         end,
+      })
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = { "markdown" },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-n>",
-          node_incremental = "<C-n>",
-          scope_incremental = "<C-s>",
-          node_decremental = "<C-m>",
+      local old_options_no_longer_supported = {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-n>",
+            node_incremental = "<C-n>",
+            scope_incremental = "<C-s>",
+            node_decremental = "<C-m>",
+          },
         },
-      },
-    })
-  end
+      }
+    end
+  },
+
+        },
 }
