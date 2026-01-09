2628b90b31f42273bed80e5620b25aa90bc58e8b
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-01-09 16:26:03 +0100

    feat(nvim): add nvim-treesitter-textobjects

--- packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/nvim-treesitter.lua
@@ -29,7 +29,7 @@ return {
       treesitter.setup({})
       treesitter.install(parsers)
 
-      vim.api.nvim_create_autocmd('FileType', {
+      vim.api.nvim_create_autocmd("FileType", {
         callback = function(args)
           if
               vim.list_contains(
@@ -38,8 +38,8 @@ return {
               )
           then
             vim.treesitter.start(args.buf)
-            -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-            -- vim.wo.foldmethod = 'expr'
+            -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            -- vim.wo.foldmethod = "expr"
             -- indentation, provided by nvim-treesitter
             vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
           end
@@ -60,5 +60,54 @@ return {
     end
   },
 
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-treesitter-textobjects").setup {
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v",
+            ["@function.outer"] = "V",
+            ["@class.outer"] = "V",
+          },
+          include_surrounding_whitespace = false,
         },
+        move = {
+          set_jumps = false,
+        },
+      }
+
+      vim.keymap.set({ "n", "x", "o" }, "]]", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start(
+          "@function.outer",
+          "textobjects"
+        )
+        vim.cmd("normal! zz")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[[", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start(
+          "@function.outer",
+          "textobjects"
+        )
+        vim.cmd("normal! zz")
+      end)
+      vim.keymap.set({ "x", "o" }, "af", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+      end)
+    end,
+  },
 }
