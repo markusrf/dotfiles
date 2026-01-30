dd3afcdc47a5deae620c93a61d5d8d3ac6272122
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-01-30 14:31:13 +0100

    feat(nvim): add gitsigns blame keymap

--- packages/neovim/.config/nvim/lua/markus/plugins/gitsigns.lua
+++ packages/neovim/.config/nvim/lua/markus/plugins/gitsigns.lua
@@ -2,6 +2,11 @@ return {
   "lewis6991/gitsigns.nvim",
   event = "BufEnter",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+        vim.keymap.set("n", "<leader>gb", gitsigns.blame, { desc = "Open git blame" })
+      end,
+    })
   end
 }
