d0b5da374f674bcfb982a9501ad0195d7c3a9ef7
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-03-11 12:10:18 +0100

    feat(nvim): add syntax highlighting for mise toml scripts

--- packages/neovim/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ packages/neovim/.config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -42,6 +42,12 @@ return {
           end
         end,
       })
+
+      require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ":t")
+        return string.match(filename, ".*mise.*%.toml$") ~= nil
+      end, { force = true, all = false })
     end,
   },
 
