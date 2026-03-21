c64c0fea47d85aac812d0252b2cbfdaae9794d35
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-03-21 15:51:19 +0100

    feat(nvim): add semver til dial

--- packages/neovim/.config/nvim/lua/custom/plugins/dial.lua
+++ packages/neovim/.config/nvim/lua/custom/plugins/dial.lua
@@ -108,6 +108,7 @@ return {
           augend.integer.alias.decimal_int, -- nonnegative and negative decimal number
           augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
           augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+          augend.semver.alias.semver,
           ordinal_numbers,
           weekdays,
           months,
