ddd9ab6016d2013ad23229497a06fc4a67e18299
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-05-05 15:14:36 +0200

    chore(nvim): disable copilot

--- packages/neovim/.config/nvim/lua/custom/plugins/copilot.lua
+++ packages/neovim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -1,30 +1,30 @@
 return {
-  "zbirenbaum/copilot.lua",
-  dependencies = { "copilotlsp-nvim/copilot-lsp" },
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        keymap = {
-          accept = "<C-enter>",
-          accept_word = "<C-S-enter>",
-          accept_line = false,
-          next = "<C-S-j>",
-          prev = "<C-S-k>",
-          dismiss = "<C-Esc>",
-        },
-      },
-      server_opts_overrides = {
-        settings = {
-          telemetry = { telemetryLevel = "off" },
-        },
-      },
-    })
-    vim.keymap.set("n", "<leader>ct", function()
-      require("copilot.suggestion").toggle_auto_trigger()
-    end, { desc = "Toggle Copilot auto suggestion" })
-  end,
+  -- "zbirenbaum/copilot.lua",
+  -- dependencies = { "copilotlsp-nvim/copilot-lsp" },
+  -- cmd = "Copilot",
+  -- event = "InsertEnter",
+  -- config = function()
+  --   require("copilot").setup({
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = false,
+  --       keymap = {
+  --         accept = "<C-enter>",
+  --         accept_word = "<C-S-enter>",
+  --         accept_line = false,
+  --         next = "<C-S-j>",
+  --         prev = "<C-S-k>",
+  --         dismiss = "<C-Esc>",
+  --       },
+  --     },
+  --     server_opts_overrides = {
+  --       settings = {
+  --         telemetry = { telemetryLevel = "off" },
+  --       },
+  --     },
+  --   })
+  --   vim.keymap.set("n", "<leader>ct", function()
+  --     require("copilot.suggestion").toggle_auto_trigger()
+  --   end, { desc = "Toggle Copilot auto suggestion" })
+  -- end,
 }
