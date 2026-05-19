456aa24354cd727774925c6774542a85bb72dcfe
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-05-19 15:55:09 +0200

    refactor(nvim): obsidian autocmd

--- packages/neovim/.config/nvim/lua/custom/plugins/obsidian.lua
+++ packages/neovim/.config/nvim/lua/custom/plugins/obsidian.lua
@@ -1,30 +1,15 @@
 local obsidian_augroup = vim.api.nvim_create_augroup("ObsidianKeymaps", { clear = true })
 
-local function create_keymap(mode, keys, func, desc)
-  vim.api.nvim_create_autocmd("User", {
-    group = obsidian_augroup,
-    pattern = "ObsidianNoteEnter",
-    callback = function(ev)
-      vim.keymap.set(mode, keys, func, {
-        buffer = ev.buf,
-        desc = desc,
-      })
-    end,
-  })
-end
+local obsidian_keymaps = {
+  { "n", "<leader>ot", "<cmd>Obsidian tags<CR>", "Obsidian: Tags" },
+  { "n", "<leader>ob", "<cmd>Obsidian backlinks<CR>", "Obsidian: Backlinks" },
+  { "n", "<leader>oh", "<cmd>Obsidian toc<CR>", "Obsidian: Headings" },
+  { "n", "<leader>or", "<cmd>Obsidian rename<CR>", "Obsidian: Rename" },
+}
 
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   config = function()
     require("obsidian").setup({
       ui = {
@@ -40,9 +25,15 @@ return {
     })
     -- TODO: v3.14 supports using lsp keybinds instead of these
     -- but the on_attach function in lsp.lua is not run when obsidian_ls starts
-    create_keymap("n", "<leader>ot", "<cmd>Obsidian tags<CR>", "Obsidian: Tags")
-    create_keymap("n", "<leader>ob", "<cmd>Obsidian backlinks<CR>", "Obsidian: Backlinks")
-    create_keymap("n", "<leader>oh", "<cmd>Obsidian toc<CR>", "Obsidian: Headings")
-    create_keymap("n", "<leader>or", "<cmd>Obsidian rename<CR>", "Obsidian: Rename")
+    vim.api.nvim_create_autocmd("User", {
+      group = obsidian_augroup,
+      pattern = "ObsidianNoteEnter",
+      callback = function(ev)
+        for _, km in ipairs(obsidian_keymaps) do
+          local mode, keys, func, desc = km[1], km[2], km[3], km[4]
+          vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = desc })
+        end
+      end,
+    })
   end,
 }
