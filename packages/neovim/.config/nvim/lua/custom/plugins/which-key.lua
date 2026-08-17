67ad0dd6c523b7bcd4f0a08daa687c3df7d562a3
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2026-08-17 12:30:17 +0200

    feat(nvim): add global which-key keymap

--- packages/neovim/.config/nvim/lua/custom/plugins/which-key.lua
+++ packages/neovim/.config/nvim/lua/custom/plugins/which-key.lua
@@ -4,4 +4,13 @@ return {
   opts = {
     delay = 300,
   },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
